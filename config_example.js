ac4e545a84b0ec4daa097f0a3079de36a50dd949
romestylez <romestylez@users.noreply.github.com>
2025-10-23 20:21:52 +0200

    Update

--- config_example.js
+++ config_example.js
@@ -1,28 +1,26 @@
 // Example configuration for Chat Overlay
 const CONFIG = {
   // Twitch
-  TWITCH_CHANNEL: "YOUR_TWITCH_NAME", 
-  TWITCH_OAUTH: "oauth:YOUR_TWITCH_OAUTH_TOKEN", // Token from https://twitchapps.com/tmi/
+  TWITCH_CHANNEL: "example_channel",
+  TWITCH_OAUTH: "oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Token from https://twitchapps.com/tmi/
 
   // Kick
-  KICK_APP_KEY: "32cbd69e4b950bf97679",
-  KICK_CLUSTER: "us2",   // e.g. "us2"
-  KICK_CHATROOM_ID: 123456,            // your chatroom ID
+  KICK_APP_KEY: "xxxxxxxxxxxxxxxxxxxx",
+  KICK_CLUSTER: "us2",
+  KICK_CHATROOM_ID: 123456,
 
   // 7TV
-  SEVENTV_USER_ID: "YOUR_7TV_USER_ID",
+  SEVENTV_USER_ID: "XXXXXXXXXXXXXXXXX",
 
   // BTTV
-  BTTV_TWITCH_USER_ID: "YOUR_TWITCH_USER_ID",
-
+  BTTV_TWITCH_USER_ID: "XXXXXXXX",
+  
   // FFZ
-  FFZ_CHANNEL: "YOUR_FFZ_CHANNEL",     // usually same as Twitch channel
-
-  // Show Twitch/Kick badges or not
+  FFZ_CHANNEL: "example_channel",
+  
+  // Show badges or not
   SHOW_BADGES: true,
-
+  
   // Blacklist: users whose messages should be ignored
-  BLOCKED_USERS: ["nightbot", "streamelements", "moobot"]
+  BLOCKED_USERS: ["examplebot1", "examplebot2", "examplebot3"]
 };
-
-
